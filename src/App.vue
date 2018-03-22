b42ff120cccd54779eb94200c092d8eb085b8eaf
yuls3 <yulishan3@gmail.com>
2018-03-22 10:46:34 +0800

    update content

--- src/App.vue
+++ src/App.vue
@@ -89,32 +89,25 @@
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">运营管理</span>
         </template>
         <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-1">数据集成</el-menu-item>
+          <el-menu-item index="1-2">流处理引擎</el-menu-item>
+          <el-menu-item index="1-3">网络爬虫</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">大数据基础能力</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
+      <el-menu-item index="3">
         <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
+        <span slot="title">大数据运维</span>
       </el-menu-item>
       <el-menu-item index="4">
         <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
+        <span slot="title">数据检索</span>
       </el-menu-item>
     </el-menu>
     <div class="container" :style="mainContainer">
